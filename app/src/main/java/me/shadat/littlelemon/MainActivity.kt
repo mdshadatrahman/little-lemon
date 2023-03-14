3d1a242b1acd8d8fbc051f1d97f1f46836385cd0
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-03-14 15:24:02 +0600

    Exercise: Defining login screen with Compose

--- app/src/main/java/me/shadat/littlelemon/MainActivity.kt
+++ app/src/main/java/me/shadat/littlelemon/MainActivity.kt
@@ -1,43 +1,58 @@
 package me.shadat.littlelemon
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import me.shadat.littlelemon.ui.theme.LittleLemonTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LittleLemonTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+            Column(
+                Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = "Little Lemon",
+                    fontSize = 32.sp,
+                    color = Color(0xFFF4CE14)
+                )
+                Text(
+                    text = stringResource(id = R.string.chicago),
+                    fontSize = 32.sp,
+                    color = Color(0xFFF4CE14)
+                )
+                Row() {
+                    Button(
+                        onClick = { },
+                        border = BorderStroke(1.dp, Color.Red),
+                        shape = RoundedCornerShape(10.dp),
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Gray)
+                        ) {
+                        Text(text = stringResource(id = R.string.order))
+                    }
                 }
             }
+
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LittleLemonTheme {
-        Greeting("Android")
     }
 }
