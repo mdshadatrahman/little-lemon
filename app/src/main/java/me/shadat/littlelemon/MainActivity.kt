3857a3fe0d4b12d1b885547b4976da9f7836fdeb
mdshadatrahman <shadat.rahman.464@gmail.com>
2023-03-25 15:19:08 +0600

    upperPanel

--- app/src/main/java/me/shadat/littlelemon/MainActivity.kt
+++ app/src/main/java/me/shadat/littlelemon/MainActivity.kt
@@ -1,24 +1,29 @@
 package me.shadat.littlelemon
 
 
+import android.nfc.Tag
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -26,33 +31,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Text(
-                    text = "Little Lemon",
-                    fontSize = 32.sp,
-                    color = Color(0xFFF4CE14)
-                )
-                Text(
-                    text = stringResource(id = R.string.chicago),
-                    fontSize = 32.sp,
-                    color = Color(0xFFF4CE14)
-                )
-                Row() {
-                    Button(
-                        onClick = { },
-                        border = BorderStroke(1.dp, Color.Red),
-                        shape = RoundedCornerShape(10.dp),
-                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Gray)
-                        ) {
-                        Text(text = stringResource(id = R.string.order))
-                    }
-                }
-            }
-
+            UpperPanel()
         }
     }
 }
---
+++ app/src/main/java/me/shadat/littlelemon/UpperPanel.kt
@@ -0,0 +1,89 @@
+package me.shadat.littlelemon
+
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun UpperPanel() {
+    val context = LocalContext.current
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .background(Color(0xFF495E57)),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start,
+    ) {
+        Text(
+            text = "Little Lemon",
+            fontSize = 32.sp,
+            color = Color(0xFFF4CE14),
+            modifier = Modifier.padding(start = 20.dp, top = 20.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.chicago),
+            fontSize = 24.sp,
+            color = Color(0xFFFFFFFF),
+            modifier = Modifier.padding(start = 20.dp)
+        )
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+            horizontalArrangement = Arrangement.Start,
+        ) {
+            Text(
+                text = stringResource(id = R.string.description_one),
+                Modifier.width(200.dp),
+                color = Color.White,
+                fontSize = 21.sp,
+
+                )
+            Image(
+                painter = painterResource(id = R.drawable.restaurant),
+                contentDescription = "",
+                Modifier
+                    .height(200.dp)
+                    .clip(RoundedCornerShape(20.dp))
+            )
+        }
+        Button(
+            onClick = {
+                Toast.makeText(context, "Order Successful!" , Toast.LENGTH_SHORT).show()
+            },
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier.padding(horizontal = 20.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFF4CE14))
+        ) {
+            Text(
+                text = stringResource(id = R.string.order_take_away),
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF333333)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DemoPreview() {
+    UpperPanel()
+}
